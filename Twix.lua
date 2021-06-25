88fafb1048fcaf8d83cc1b35602fba53c9c8def4
Twixsr <86269429+Twixsr@users.noreply.github.com>
2021-06-25 12:37:33 +0300

    Add files via upload

--- Twix.lua
+++ Twix.lua
@@ -6425,7 +6425,7 @@ keyboard.inline_keyboard = {
 },
 }
 local msg_id = msg.id_/2097152/0.5
-local texte = '▹ |'..Description..'\n▹ |ايديك  . '..Id..'\n⌔︙معرفك  . '..UserName_User..'\n▹ |رتبتك  . '..Status_Gps..'\n⌔︙رسائلك  . '..NumMsg..' \n⌔︙التفاعل . '..TotalMsg..'\n▹ |الالعاب  . '..Num_Games
+local texte = '⌔︙'..Description..'\n⌔︙ايديك  . '..Id..'\n⌔︙معرفك  . '..UserName_User..'\n⌔︙رتبتك  . '..Status_Gps..'\n⌔︙رسائلك  . '..NumMsg..' \n⌔︙التفاعل . '..TotalMsg..'\n⌔︙الالعاب  . '..Num_Games
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id='..msg.chat_id_..'&caption='..URL.escape(texte)..'&photo='..taha.photos_[0].sizes_[1].photo_.persistent_id_..'&reply_to_message_id='..msg_id..'&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard)) 
 end
 else
@@ -6455,7 +6455,7 @@ local texte = '['..get_id..']'
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(texte).."&reply_to_message_id="..msg_id.."&parse_mode=markdown")
 else
-local texte = '\n*▹ |ايديك  . '..Id..'\n⌔︙معرفك  .* ['..UserName_User..']*\n▹ |رتبتك  . '..Status_Gps..'\n⌔︙رسائلك  . '..NumMsg..' \n⌔︙التفاعل . '..TotalMsg..'\n▹ |الالعاب  . '..Num_Games..'*'
+local texte = '\n*⌔︙ايديك  . '..Id..'\n⌔︙معرفك  .* ['..UserName_User..']*\n⌔︙رتبتك  . '..Status_Gps..'\n⌔︙رسائلك  . '..NumMsg..' \n⌔︙التفاعل . '..TotalMsg..'\n⌔︙الالعاب  . '..Num_Games..'*'
 keyboard = {} 
 keyboard.inline_keyboard = {
 {
@@ -6515,7 +6515,7 @@ local Status_Gps = database:get(bot_id.."Twix:Comd:New:rt:User:"..msg.chat_id_..
 local message_edit = database:get(bot_id..'Twix:message_edit'..msg.chat_id_..data.id_) or 0
 local Num_Games = database:get(bot_id.."Tshak:Msg_User"..msg.chat_id_..":"..data.id_) or 0
 local Add_Mem = database:get(bot_id.."Twix:Add:Memp"..msg.chat_id_..":"..data.id_) or 0
-local texte = '\n*▹ |ايديك  . '..Id..'\n⌔︙معرفك  .* ['..UserName_User..']*\n▹ |رتبتك  . '..Status_Gps..'\n⌔︙رسائلك  . '..NumMsg..' \n⌔︙التفاعل . '..TotalMsg..'\n▹ |الالعاب  . '..Num_Games..'*'
+local texte = '\n*⌔︙ايديك  . '..Id..'\n⌔︙معرفك  .* ['..UserName_User..']*\n⌔︙رتبتك  . '..Status_Gps..'\n⌔︙رسائلك  . '..NumMsg..' \n⌔︙التفاعل . '..TotalMsg..'\n⌔︙الالعاب  . '..Num_Games..'*'
 keyboard = {} 
 keyboard.inline_keyboard = {
 {
@@ -6547,7 +6547,7 @@ local Status_Gps = database:get(bot_id.."Twix:Comd:New:rt:User:"..msg.chat_id_..
 local message_edit = database:get(bot_id..'Twix:message_edit'..msg.chat_id_..data.id_) or 0
 local Num_Games = database:get(bot_id.."Tshak:Msg_User"..msg.chat_id_..":"..data.id_) or 0
 local Add_Mem = database:get(bot_id.."Twix:Add:Memp"..msg.chat_id_..":"..data.id_) or 0
-local texte = '\n*▹ |ايديك  . '..Id..'\n⌔︙معرفك  .* ['..UserName_User..']*\n▹ |رتبتك  . '..Status_Gps..'\n⌔︙رسائلك  . '..NumMsg..' \n⌔︙التفاعل . '..TotalMsg..'\n▹ |الالعاب  . '..Num_Games..'*'
+local texte = '\n*⌔︙ايديك  . '..Id..'\n⌔︙معرفك  .* ['..UserName_User..']*\n⌔︙رتبتك  . '..Status_Gps..'\n⌔︙رسائلك  . '..NumMsg..' \n⌔︙التفاعل . '..TotalMsg..'\n⌔︙الالعاب  . '..Num_Games..'*'
 keyboard = {} 
 keyboard.inline_keyboard = {
 {
